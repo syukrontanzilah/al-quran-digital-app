67bca741574cec8d30966ba04d1772f1e705e5f8
syukrontanzilah <syukrontanzilah@gmail.com>
2020-06-28 06:46:45 +0700

    initial first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hallo ini halaman awal mau membuat aplikasi quran</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
