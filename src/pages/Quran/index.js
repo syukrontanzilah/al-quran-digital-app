c6dc3fc9645239428a42f9cfeb1f9da568567554
syukrontanzilah <syukrontanzilah@gmail.com>
2020-06-28 12:55:20 +0700

    styling page quraan

--- src/pages/Quran/index.js
+++ src/pages/Quran/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { Text, View, ScrollView } from 'react-native'
+import { Text, View, ScrollView, StyleSheet } from 'react-native'
 import SkeletonContent from "react-native-skeleton-content-nonexpo";
 import Axios from 'axios';
-import { ListItem } from 'react-native-elements';
-import {fonts} from '../../utils'
+import { ListItem, colors } from 'react-native-elements';
+import { fonts } from '../../utils'
 
 
 export default class Quran extends Component {
@@ -12,7 +12,7 @@ export default class Quran extends Component {
         super(props)
         this.state = {
             hasil: [],
-            isLoading : true,
+            isLoading: true,
         }
     }
 
@@ -21,7 +21,7 @@ export default class Quran extends Component {
             const response = await Axios.get('https://al-quran-8d642.firebaseio.com/data.json');
             this.setState({
                 hasil: response.data,
-                isLoading:false
+                isLoading: false
             })
             console.log(this.state);
         } catch (error) {
@@ -36,35 +36,44 @@ export default class Quran extends Component {
 
     render() {
         return (
-            <ScrollView style={{flex:1, backgroundColor: '#fff'}}>
-                <SkeletonContent 
-                    containerStyle={{flex:1}}
+            <ScrollView style={styles.page}>
+                <SkeletonContent
+                    containerStyle={{ flex: 1, paddingHorizontal: 10 }}
                     isLoading={this.state.isLoading}
                     layout={[
-                        { key: "someId", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId2", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId3", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId4", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId5", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId6", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId7", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId8", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
-                        { key: "someId9", width: 350, height: 60, marginVertical:5,marginHorizontal:5 },
+                        { key: "someId", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId2", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId3", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId4", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId5", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId6", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId7", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId8", width: '100%', height: 60, marginVertical: 5, },
+                        { key: "someId9", width: '100%', height: 60, marginVertical: 5, },
                     ]}
                 >
                     {
                         this.state.hasil.map((item, i) => (
                             <ListItem
                                 key={i}
-                                title={item.nama}
-                                leftElement={item.nomor}
+                                title={item.nomor+'. ' + item.nama }
+                                titleStyle={styles.nama}
+
+
+                                //leftElement={item.nomor}
+                                //leftElementStyle={styles.nomor}
+
                                 rightSubtitle={item.asma}
-                                rightSubtitleStyle={{ fontSize: 30, fontWeight: 'bold', fontFamily: fonts.arab }}
+                                rightSubtitleStyle={styles.textArab}
+
                                 subtitle={item.arti + ' (' + item.ayat + ')'}
+                                subtitleStyle={styles.subtitle}
+
+
                                 bottomDivider
-                                onPress={() => this.props.navigation.navigate('Surat',{
-                                    noSurat : item.nomor,
-                                    namaSurat : item.nama
+                                onPress={() => this.props.navigation.navigate('Surat', {
+                                    noSurat: item.nomor,
+                                    namaSurat: item.nama
                                 })}
                             />
                         ))
@@ -74,3 +83,25 @@ export default class Quran extends Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    nama: {
+        fontSize: 17,
+        color: 'green',
+        fontFamily: fonts.bold
+    },
+
+    textArab: {
+        fontSize: 35,
+        fontFamily: fonts.arab,
+        color: colors.green
+    },
+    subtitle: {
+        fontSize: 14,
+        fontFamily: fonts.regular, 
+    }
+})
